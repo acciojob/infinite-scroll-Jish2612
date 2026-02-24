8cae16f3640129439596345becb0813573e94712
Jish2612 <167243813+Jish2612@users.noreply.github.com>
2026-02-24 21:26:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here!
+const list = document.getElementById("infi-list");
 
+let count = 1;
+
+// Add initial 10 items
+for (let i = 0; i < 10; i++) {
+  list.innerHTML += `<li>Item ${count++}</li>`;
+}
+
+// Detect scroll to bottom
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight - 5) {
+    // Add 2 more items
+    for (let i = 0; i < 2; i++) {
+      list.innerHTML += `<li>Item ${count++}</li>`;
+    }
+  }
+});
